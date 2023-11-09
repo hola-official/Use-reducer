73f1384ee56636337c1c480712863b6cade7aff1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 15:19:04 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useReducer, useState } from "react";
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return {...state, count: state.count + 1 }
+      return { ...state, count: state.count + 1 }
     case "decrement":
-      return {...state, count: state.count - 1 }
+      return { ...state, count: state.count - 1 }
     case 'newUserInput':
-      return {...state, userInput: action.payload }
+      return { ...state, userInput: action.payload }
     case 'tgColor':
-      return {...state, color: !state.color }
+      return { ...state, color: !state.color }
     default:
       throw new Error();
   }
