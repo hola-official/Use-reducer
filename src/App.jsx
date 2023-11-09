fc852ed505e0f0ed77797bfff0350374a1bcb762
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-09 14:59:35 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
       return {count: state.count + 1}
+    case "decrement":
+      return {count: state.count - 1}
   }
 }
 
